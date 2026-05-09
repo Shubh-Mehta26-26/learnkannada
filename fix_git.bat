2d2019399638f8ac810b178c1ad86816d9799dde
Shubh-Mehta26-26 <shubhmehta2604@gmail.com>
2026-05-09 07:20:45 +0530

    first upload

--- fix_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Running git rm... > git_fix.log
-git rm -r --cached node_modules >> git_fix.log 2>&1
-echo Running git add... >> git_fix.log
-git add .gitignore >> git_fix.log 2>&1
-echo Running git commit... >> git_fix.log
-git commit -m "fix: remove node_modules and add .gitignore" >> git_fix.log 2>&1
-echo Done. >> git_fix.log
